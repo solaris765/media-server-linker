6f4ad97e8912c34b8ab16513fdedb26fde760f16
Mason Rhodes <mason.rhodes@outlook.com>
2024-02-14 00:29:12 -0800

    Refactor createHardLink to createSymLink using fs/promises

--- src/util/filesystem.ts
+++ src/util/filesystem.ts
@@ -1,34 +1,53 @@
-import * as fs from 'fs';
+import fs from 'fs/promises';
 import pouchdb from 'pouchdb';
 
 
-export interface DBEntry {
+export interface DBEntryLike {
   _id: string;
+  _rev?: string;
   realPath: string;
   mediaServers: Record<string, string>;
 }
+export class DBEntry {
+  _id: string;
+  _rev?: string;
+  realPath: string;
+  mediaServers: Record<string, string>;
 
-export async function createHardLink(source: string, destination: string): Promise<void> {
+  constructor(id: string, realPath: string, mediaServers: Record<string, string>, rev?:string) {
+    this._id = id;
+    this._rev = rev;
+    this.realPath = realPath;
+    this.mediaServers = mediaServers;
+  }
+
+  static fromDoc(doc: DBEntryLike): DBEntry {
+    return new DBEntry(doc._id, doc.realPath, doc.mediaServers, doc._rev);
+  }
+
+  toDoc(): DBEntryLike {
+    return {
+      _id: this._id,
+      _rev: this._rev,
+      realPath: this.realPath,
+      mediaServers: this.mediaServers,
+    };
+  }
+
+  updateMediaServerPath(mediaServerPath: string, path: string|undefined) {
+    if (path === undefined) {
+      delete this.mediaServers[mediaServerPath];
+    } else {
+    this.mediaServers[mediaServerPath] = path;  
+    }
+  }
+}
+
+export async function createSymLink(source: string, destination: string): Promise<void> {
   // ensure the destination directory exists creating its parent directories if necessary
   const destinationDirectory = destination.split('/').slice(0, -1)
 
-  await new Promise<void>((resolve, reject) => {
-    fs.mkdir(destinationDirectory.join('/'), { recursive: true }, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+  await fs.mkdir(destinationDirectory.join('/'), { recursive: true });
 
-  return new Promise((resolve, reject) => {
-    fs.link(source, destination, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+  return fs.link(source, destination);
 }
