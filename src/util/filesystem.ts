b3de390201c1231c0bbd061b0eabd2b6fa82b6b2
Mason Rhodes <mason@lifemd.com>
2024-02-19 15:23:51 -0800

    Refactor getFilesFromMediaPath to return only file paths

--- src/util/filesystem.ts
+++ src/util/filesystem.ts
@@ -34,5 +34,7 @@ export async function removeLink(linkPath: string): Promise<void> {
 }
 
 export async function getFilesFromMediaPath(path:string): Promise<string[]> {
-  return glob(`${path}/**/*`);
+  return glob(`${path}/**/*`,{withFileTypes: true}).then((files) => {
+    return files.filter((file) => file.isFile()).map((file) => file.path)
+  });
 }
